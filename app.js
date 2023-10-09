51bf3f30329300b09b2853e928f8997c1b11aa2b
Al-Howari95 <m.howari95@gmail.com>
2023-10-09 11:43:30 +0300

    school-system

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-// // table
+// table
 // document.getElementById("form").addEventListener("submit", function(event) {
 //     event.preventDefault(); 
 
@@ -48,137 +48,91 @@
 // ----------------------------------------------------------------
 
 
-
-
-// function Student(fullName, birthday, number, bir, Gender, Major, ImageURL) {
-
-//     this.fullName = fullName; 
-//     this.birthday = birthday;
-//     this.number = number;
-//     this.bir = bir;
-//     this.Gender = Gender;
-//     this.Major = Major;
-//     this.ImageURL = ImageURL;
-// }
-
-// document.getElementById("form").addEventListener("submit", function(event) {
-//     event.preventDefault(); 
-
-// let fullName = document.getElementById("username").value;
-// let g = document.getElementById("grade").value;
-// let number = document.getElementById("phonenumber").value;
-// let bir = document.getElementById("birthday").value;
-// let gender;
-
-// if ( document.getElementById("Male").checked) {
-//     gender = "Male";
-
-// } else if (document.getElementById("Female").checked) {
-//     gender = "Female";
-// }
-
-// let Major;
-
-// if ( document.getElementById("Administration").checked) {
-//     Major = "Administration";
-
-// } else if (document.getElementById("IT").checked) {
-//     Major = "IT";
-    
-// } else if (document.getElementById("Science").checked) {
-//     Major = "Science";
-
-
-// }else if (document.getElementById("Art").checked) {
-//     Major = "Art";
-
-// }
-
-
-// });
-
-
-
-
-
-
-
 // constructor
 
-class School {
 
-    constructor (name,date,Phone,grade) {     
-        this.name = name;
+class Student {
+    constructor(Name, gender, number, date, Image , Major) {
+        this.Name = Name;
+        this.gender = gender;
+        this.number = number;
         this.date = date;
-        // this.gender = gender;
-        this.Phone = Phone;
-        this.grade = grade;
+        this.Image = Image;
+        this.Major = Major;
     }
-    functionSchool(...a){
-        let arr=[];
-
-        arr.push(a);
-        console.log(arr)
-      
-          localStorage.setItem("Students", JSON.stringify(a));
-      }
- }
-
-let submit= document.getElementById("S")
- submit.addEventListener("click" , (eventt) => {
-    eventt.preventDefault();
-
-    let card = document.createElement("div");
-    card.style.border = "3px solid black";
-    card.style.marginLeft="15px"
-    card.style.marginTop="10px"
-    card.style.width="120px"
+}
 
 
-    let image = document.createElement("img");
-    image.src="img/profile.jpg"
-   
-
-    let name= document.createElement("div")
-    let date= document.createElement("div")
-    // let gender= document.createElement("div")
-    let Phone= document.createElement("div")
-    let grade= document.createElement("div")
+function saveForm(student) {
+    let students = JSON.parse(localStorage.getItem('students')) || [];
+    students.push(student);
+    localStorage.setItem('students', JSON.stringify(students));
+}
 
 
+function displaySavedData() {
+    let students = JSON.parse(localStorage.getItem('students')) || [];
 
-    name.innerHTML=document.getElementById("username").value;
-    date.innerHTML=document.getElementById("birthday").value;
-    // gender.innerHTML=document.getElementsByName("gender").value;
-    Phone.innerHTML=document.getElementById("phonenumber").value;
-    grade.innerHTML=document.getElementById("grade").value;
- 
+    let result = document.getElementById("cardSIn");
+    result.innerHTML = '';
 
+    students.forEach(student => {
+        let card = document.createElement("div");
+        card.style.border = "3px solid black";
+        card.style.marginLeft = "20px";
+        card.style.marginTop = "10px";
+        card.style.width = "250px";
+        card.style.height= "250px";
 
+        let image = document.createElement("img");
+        image.src = "img/profile.jpg"; 
 
-   let sName = document.getElementById("username").value;
-   let aDate = document.getElementById("birthday").value;
-//    let nGender = document.getElementById("gender").value;
-   let gPhone = document.getElementById("phonenumber").value;
-   let lGrade = document.getElementById("grade").value;
+        let name = document.createElement("p");
+        let datea = document.createElement("p");
+        let Phone = document.createElement("p");
+        let gender = document.createElement("p");
 
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(datea);
+        card.appendChild(Phone);
+        card.appendChild(gender);
 
+        name.innerHTML = `Name : ${student.Name}`;
+        datea.innerHTML = `Date : ${student.date}`;
+        Phone.innerHTML = `Phone number : ${student.number}`;
+        gender.innerHTML = `Gender : ${student.gender}`;
 
-   
+        result.appendChild(card);
+    });
+}
 
-   let ca = new School(name,date,Phone,grade);   
-   ca.functionSchool([sName,aDate,gPhone,lGrade]);
+document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    card.appendChild(image);
-    card.appendChild(name);
-    card.appendChild(date);
-    // card.appendChild(gender);
-    card.appendChild(Phone);
-    card.appendChild(grade);
+    let Name = document.getElementById("username").value;
 
+    let gender;
 
-    let cardSIn = document.getElementById("cardSIn");
-    
-    cardSIn.appendChild(card);
+    if (document.getElementById("Male").checked) {
+        gender = "Male";
+    } else if (document.getElementById("Female").checked) {
+        gender = "Female";
+    }
 
+    let number = document.getElementById("phonenumber").value;
+    let date = document.getElementById("birthday").value;
+    let Image = document.getElementById("image-url").value;
+    // let Majors = document.getElementsByName("c")
+
+    let student = new Student(Name, gender, number, date, Image);
+
+    saveForm(student);
+    displaySavedData();
 });
+
+
+displaySavedData();
+
+
+
